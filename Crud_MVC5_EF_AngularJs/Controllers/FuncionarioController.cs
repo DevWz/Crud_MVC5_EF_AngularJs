390b3b01be2d3f43e5f5f6a8bcec54c45f717188
DevWz <wellingtonantonio_zaneze@live.com>
2017-12-28 23:26:27 -0200

    Instalando o AngularJs e criando o controller FuncionarioController

---
+++ Crud_MVC5_EF_AngularJs/Controllers/FuncionarioController.cs
@@ -0,0 +1,22 @@
+﻿using Crud_MVC5_EF_AngularJs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_MVC5_EF_AngularJs.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public JsonResult GetFuncionario()
+        {
+            using (var db = new FuncionariosEntities())
+            {
+                List<Funcionario> listarFuncionarios = db.Funcionarios.ToList();
+                return Json(listarFuncionarios);
+            }
+        }
+    }
+}
